b91481428119819a56e410f2d6be9003bd8f0982
kokopelli314 <nacclonts@gmail.com>
2017-07-11 23:02:02 -0600

    market detail view: combining local data with open gov data source

--- markets/api.py
+++ markets/api.py
@@ -1,4 +1,5 @@
 import requests
+import urllib.parse
 
 BASE_API_URL = 'http://127.0.0.1:5000/yourmarket/api/'
 def getMarketData(id):
@@ -7,3 +8,10 @@ def getMarketData(id):
     """
     response = requests.get(BASE_API_URL + 'id/' + id)
     return response.json()
+
+def getMapsLink(market):
+    args = '{0},{1} ("{2}")'
+    args = args.format(market['y'], market['x'], market['MarketName'])
+    args = urllib.parse.quote(args, '()')
+    url = 'https://maps.google.com/?q=' + args
+    return url
--- markets/views.py
+++ markets/views.py
@@ -1,3 +1,4 @@
+from django.forms.models import model_to_dict
 from django.shortcuts import render, get_object_or_404
 
 from . import api
@@ -14,16 +15,22 @@ def index(request):
     )
 
 def market_detail(request, market_id):
+    """
+    Detailed view of a single market.
+    """
+    opengov_data = api.getMarketData(market_id)
+    opengov_data['MapsLink'] = api.getMapsLink(opengov_data)
     try:
         market = Market.objects.get(id=market_id)
     except Market.DoesNotExist:
         print('making a market!')
-        data = api.getMarketData(market_id)
         market = Market(id=market_id,
-                        name=data['MarketName'],
-                        address_street=data['street'],
-                        address_city=data['city'],
-                        address_state=data['State'],
-                        address_zip=data['zip'])
+                        name=opengov_data['MarketName'],
+                        address_street=opengov_data['street'],
+                        address_city=opengov_data['city'],
+                        address_state=opengov_data['State'],
+                        address_zip=opengov_data['zip'])
         market.save()
-    return render(request, 'markets/detail.html', {'market': market})
+
+    market_merged = {**model_to_dict(market), **opengov_data}
+    return render(request, 'markets/detail.html', {'market': market_merged})
