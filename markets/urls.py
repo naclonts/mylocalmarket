7ff5c94c45d48b82c9250ae6b7b476659309d917
kokopelli314 <nacclonts@gmail.com>
2017-07-22 20:42:32 -0600

    initial login template

--- markets/urls.py
+++ markets/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    # url(r'^market/(?P<pk>\d+)$', views.MarketDetailView.as_view(), name='market_detail'),
-    url(r'^market/(?P<market_id>\d+)$', views.market_detail, name='market_detail'),
+    url(r'^market/(?P<market_id>\d+)$', views.market_detail, name='detail'),
     url(r'^zip/(?P<zip>\d+)$', views.markets_within_zip, name='markets_within_zip'),
     url(r'^', views.index, name='home'),
 ]
--- mylocalmarket/urls.py
+++ mylocalmarket/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('markets.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls', namespace='auth')),
+    url(r'^', include('markets.urls', namespace='markets')),
 ]
