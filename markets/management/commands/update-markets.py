89a05ca7bf35d094a9ee266c475d6e2eeb387868
kokopelli314 <nacclonts@gmail.com>
2018-04-01 10:14:25 -0600

    add boolean fields to markets showing products offered

--- markets/management/commands/update-markets.py
+++ markets/management/commands/update-markets.py
@@ -1,7 +1,7 @@
 """
 Custom command to refresh Markets models.
 
-This command loads a CSV file from the opengov sources [TODO: load from site!] and updates the markets table to match the fields in said CSV.
+This command loads a CSV file from the opengov sources and updates the markets table to match the fields in said CSV.
 """
 
 from django.core.management.base import BaseCommand, CommandError
@@ -15,9 +15,9 @@ import os.path
 
 
 MARKET_FIELDS_CSV_TO_MODEL = {
+    # Farmer market name and location
     'FMID': 'id',
     'MarketName': 'name',
-    'Website': 'website',
     'street': 'address_street',
     'city': 'address_city',
     'State': 'address_state',
@@ -25,6 +25,55 @@ MARKET_FIELDS_CSV_TO_MODEL = {
     'zip': 'address_zip',
     'x': 'longitude',
     'y': 'latitude',
+
+    # Timeswebsite
+    # 'Season1Date': 'season_date_1',
+    # 'Season1Time': 'season_time_1',
+    # 'Season2Date': 'season_date_2',
+    # 'Season2Time': 'season_time_2',
+
+    # Links
+    'Website': 'website',
+    'Facebook': 'facebook',
+    'Twitter': 'twitter',
+    'Youtube': 'youtube',
+    'OtherMedia': 'other_media',
+
+    # Boolean fields
+    'Bakedgoods': 'has_baked_goods',
+    'Cheese': 'has_cheese',
+    'Crafts': 'has_crafts',
+    'Flowers': 'has_flowers',
+    'Eggs': 'has_eggs',
+    'Seafood': 'has_seafood',
+    'Herbs': 'has_herbs',
+    'Vegetables': 'has_vegetables',
+    'Honey': 'has_honey',
+    'Jams': 'has_jams',
+    'Meat': 'has_meat',
+    'Nursery': 'has_nursery',
+    'Nuts': 'has_nuts',
+    'Plants': 'has_plants',
+    'Poultry': 'has_poultry',
+    'Prepared': 'has_prepared_food',
+    'Soap': 'has_soap',
+    'Trees': 'has_trees',
+    'Wine': 'has_wine',
+    'Coffee': 'has_coffee',
+    'Beans': 'has_beans',
+    'Fruits': 'has_fruits',
+    'Grains': 'has_grains',
+    'Juices': 'has_juices',
+    'Mushrooms': 'has_mushrooms',
+    'PetFood': 'has_petfood',
+    'Tofu': 'has_tofu',
+    'WildHarvested': 'has_wild_harvested',
+    'Organic': 'has_organic',
+    'Credit': 'has_credit',
+    'WIC': 'has_wic',
+    'WICcash': 'has_wic_cash',
+    'SFMNP': 'has_sfmnp',
+    'SNAP': 'has_snap'
 }
 
 
@@ -56,7 +105,7 @@ class Command(BaseCommand):
                         value = data[i].strip()
                         if value != '' and field in MARKET_FIELDS_CSV_TO_MODEL.keys():
                             model_field = MARKET_FIELDS_CSV_TO_MODEL[field]
-                            setattr(market, model_field, value)
+                            setattr(market, model_field, clean_value(value))
                     market.save()
         self.stdout.write(self.style.SUCCESS('Successfully updated markets database from CSV.'))
 
@@ -71,3 +120,12 @@ class Command(BaseCommand):
             # query = query.format(','.join(columns), ','.join('?' * len(columns)))
             # for data in reader:
             #     cursor.execute(query, data)
+
+
+# Clean a value from the input CSV file
+# Converts Y/N booleans to actual Booleans
+def clean_value(value):
+    if value == 'Y': return True
+    if value == 'N': return False
+    if value == '-': return None
+    return value
